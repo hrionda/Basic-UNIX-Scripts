3f4d25c1a33885ef814392a46d2411ca09ddad1e
Hector Rionda <53663640+hrionda@users.noreply.github.com>
2020-10-24 21:17:40 -0400

    Update delete.sh

--- SysAdminScripts/delete.sh
+++ SysAdminScripts/delete.sh
@@ -1,32 +1,15 @@
-#MARIADB
-
 #CentOS
+systemctl start mariadb
+systemctl start mongod
+
 while IFS=, read -r USERNAME FNAME LNAME GENDER DOB COUNTRY COLOR FRUIT OS SHELL PERMISSION
 do
 	if [ $OS = "mac" ]
 	then
+		#MariaDB
 		mysql -e "DELETE FROM $COUNTRY WHERE uid = '$USERNAME'" country
-		mysql -e "DELETE FROM $FRUIT WHERE uid = '$FRUIT'" fruits
-	fi
-done <"/root/hrion001.csv"
-
-#Fedora
-while IFS=, read -r USERNAME FNAME LNAME GENDER DOB COUNTRY COLOR FRUIT OS SHELL PERMISSION
-do
-	if [ $OS = "mac" ]
-	then
-		mysql -e "DELETE FROM $COLOR WHERE uid = '$USERNAME'" color
-		mysql -e "DELETE FROM $GENDER WHERE uid = '$FRUIT'" gender
-	fi
-done <"/root/hrion001.csv"
-
-#MONGODB
-
-#CentOS
-while IFS=, read -r USERNAME FNAME LNAME GENDER DOB COUNTRY COLOR FRUIT OS SHELL PERMISSION
-do
-	if [ $OS = "mac" ]
-	then
+		mysql -e "DELETE FROM $FRUIT WHERE uid = '$USERNAME'" fruits
+		#MongoDB
 		echo "use country
 		db.$COUNTRY.remove({'uid':'$USERNAME'})
 		use fruits
@@ -34,13 +17,19 @@ do
 		mongo < /root/mongopart.js
 	fi
 done <"/root/hrion001.csv"
-rm /root/mongopart.js
 
 #Fedora
+systemctl start mariadb
+systemctl start mongod
+
 while IFS=, read -r USERNAME FNAME LNAME GENDER DOB COUNTRY COLOR FRUIT OS SHELL PERMISSION
 do
 	if [ $OS = "windows" ]
 	then
+		#MariaDB
+		mysql -e "DELETE FROM $COLOR WHERE uid = '$USERNAME'" color
+		mysql -e "DELETE FROM $GENDER WHERE uid = '$USERNAME'" gender
+		#MongoDB
 		echo "use color
 		db.$COLOR.remove({'uid':'$USERNAME'})
 		use gender
@@ -48,4 +37,3 @@ do
 		mongo < /root/mongopart.js
 	fi
 done <"/root/hrion001.csv"
-rm /root/mongopart.js
